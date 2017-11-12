8c3f5024b062cd6eb7d8ae88d865f3921d436338
Mitsu <mono0423@gmail.com>
2017-11-12 21:16:17 +0900

    クラスコメント、メソッドコメントを付与

--- PG_Test_2017_Autumn/src/main/java/jp/pgtest_autumn/bean/Photo.java
+++ PG_Test_2017_Autumn/src/main/java/jp/pgtest_autumn/bean/Photo.java
@@ -1,17 +1,42 @@
 package jp.pgtest_autumn.bean;
 
+/**
+ * 写真クラス。(修正不要)
+ * <p>
+ * 簡単のため、写真の情報はファイル名しか保持しない。<br>
+ * ファイル名は"yyyyMMdd_hhmmss.png"とする。(例：20171121_173000.png)
+ * </p>
+ *
+ * @author SYS Takahiro Mitsuoka
+ */
 public class Photo {
 
+	/** ファイル名 */
 	private String fileName;
 
+	/**
+	 * コンストラクタ。
+	 *
+	 * @param fileName ファイル名
+	 */
 	public Photo(String fileName) {
 		this.fileName = fileName;
 	}
 
+	/**
+	 * ファイル名取得メソッド。
+	 *
+	 * @return ファイル名
+	 */
 	public String getFileName() {
 		return fileName;
 	}
 
+	/**
+	 * 拡張子付きファイル名取得メソッド。
+	 *
+	 * @return ファイル名(拡張子付き)
+	 */
 	public String getFileNameWithExtension() {
 		return fileName + ".png";
 	}
--- PG_Test_2017_Autumn/src/main/java/jp/pgtest_autumn/model/PhotoListManager.java
+++ PG_Test_2017_Autumn/src/main/java/jp/pgtest_autumn/model/PhotoListManager.java
@@ -5,23 +5,49 @@ import java.util.List;
 
 import jp.pgtest_autumn.bean.Photo;
 
+/**
+ * 写真リスト管理クラス。(修正不要)
+ *
+ * @author SYS Takahiro Mitsuoka
+ */
 public class PhotoListManager {
 
-	private List<Photo> photoListManager;
+	/** 写真リスト */
+	private List<Photo> photoList;
 
+	/**
+	 * コンストラクタ。
+	 */
 	public PhotoListManager() {
-		photoListManager = new ArrayList<>();
+		photoList = new ArrayList<>();
 	}
 
+	/**
+	 * 写真追加メソッド。
+	 * <p>
+	 * 写真リストに引数に受け取った写真オブジェクトを追加する。
+	 * </p>
+	 * @param photo 写真オブジェクト
+	 */
 	public void add(Photo photo) {
-		photoListManager.add(photo);
+		photoList.add(photo);
 	}
 
+	/**
+	 * 写真リスト取得メソッド。
+	 *
+	 * @return 写真リスト
+	 */
 	public List<Photo> getPhotoList() {
-		return photoListManager;
+		return photoList;
 	}
 
+	/**
+	 * 写真0枚判定メソッド。
+	 *
+	 * @return true(写真リストが空)、false(写真リストのサイズが1以上)
+	 */
 	public boolean isEmpty() {
-		return photoListManager.isEmpty();
+		return photoList.isEmpty();
 	}
 }

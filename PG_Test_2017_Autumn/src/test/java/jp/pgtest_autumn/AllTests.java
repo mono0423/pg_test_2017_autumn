c5cbfe54fa6f5ea92a3ffe54636681b72636bfee
mono0423 <mono0423@gmail.com>
2017-11-28 18:41:46 +0900

    テストスイート追加

--- PG_Test_2017_Autumn/src/test/java/jp/pgtest_autumn/AllTests.java
+++ PG_Test_2017_Autumn/src/test/java/jp/pgtest_autumn/AllTests.java
@@ -1,6 +1,5 @@
 package jp.pgtest_autumn;
 
-import org.junit.runner.JUnitCore;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
@@ -18,14 +17,4 @@ import jp.pgtest_autumn.factory.ApplicationFactoryTest;
 	ApplicationFactoryTest.class
 })
 public class AllTests {
-
-	public static int albumScore = 0;
-	public static int cameraScore = 0;
-	public static int factoryScore = 0;
-	public static int controllerScore = 0;
-
-	public static void main(String[] args) {
-		JUnitCore.main(AllTests.class.getName());
-		System.out.println(albumScore);
-	}
 }
